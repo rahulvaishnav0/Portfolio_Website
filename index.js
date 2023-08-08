1f030de704b0afea6c4bbe3b7e5505c6c1fba0de
rahulvaishnav0 <141699717+rahulvaishnav0@users.noreply.github.com>
2023-08-08 18:08:55 +0530

    added files

---
+++ index.js
@@ -0,0 +1,7 @@
+var Typed = new typed('text' , {
+    Strings: ["Frontend Developer" , "YouTuber" , "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+});
